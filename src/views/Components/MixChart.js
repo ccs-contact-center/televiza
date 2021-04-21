99c1834cc99da1e9ff80259d55b1af269c0481bd
DanielDO93 <danieldionisio1993@gmail.com>
2021-04-21 18:46:04 -0500

    Update MixChart.js

--- src/views/Components/MixChart.js
+++ src/views/Components/MixChart.js
@@ -37,7 +37,24 @@ const options = {
   "DIANA LAURA  GOYRE GARCIA ", "MARIANA  FLORES  HERNANDEZ", "PATRICIA  JIMENEZ  JUAREZ", "LAURA GISELLE ROBLES  MEJIA",
    "TERESA  TOVAR TREJO"],
 	tooltips: {
-		mode: 'label'
+		mode: 'label',
+		callbacks: {
+			label: function(tooltipItem, data) {
+			  //get the concerned dataset
+			  var dataset = data.datasets[tooltipItem.datasetIndex];
+			  //calculate the total of this data set
+			  var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+				return previousValue + currentValue;
+			  });
+			  //get the current items value
+			  var currentValue = dataset.data[tooltipItem.index];
+			  //calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
+			  var percentage = Math.floor(((currentValue/total) * 100)+0.5);
+		
+			  return percentage + "%";
+			}
+		  }
+
 	},
 	elements: {
 		line: {
