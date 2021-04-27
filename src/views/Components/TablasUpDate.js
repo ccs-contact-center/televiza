497ff855d369d970b1057a3d9c4979fdbece5e98
DanielDO93 <danieldionisio1993@gmail.com>
2021-04-27 17:50:29 -0500

    Update TablasUpDate.js

--- src/views/Components/TablasUpDate.js
+++ src/views/Components/TablasUpDate.js
@@ -3,9 +3,9 @@ import {
     Container,
     Row,
     Col,
-  } from "reactstrap";
+} from "reactstrap";
 
-  import {
+import {
     Tabla1,
     Tabla2,
     TablaK1,
@@ -13,7 +13,10 @@ import {
     Tabla3,
     TablaI3,
     Tabla4, TablaRVN4,
-  } from "./Tablas";
+    TablaCalidad8,
+    Tabla8,
+} from "./Tablas";
+import { BarChart8, BarChart9,BarChartMar9, BarChartDoble } from "./Graphics";
 import React, { Component } from "react";
 
 
@@ -120,4 +123,113 @@ class ResumenVentasNuevas extends Component {
     }
 }
 
-export  {Kpis,IndicadoresAtencion, ResumenVentasNuevas}
+class CalidadMarzo1 extends Component {
+    render() {
+        return (
+            <div >
+                <div className="slide">
+                    <Container>
+                        <Row>
+                            <Col xs="12">
+                                <h2 className="border border-bottom border-danger"><span>Calidad</span></h2>
+                                <Tabla8 />
+                            </Col>
+                            <Col xs="12">
+                                <p className="text-lef"><strong>Con un promedio ABC de:</strong> 93.02%.</p>
+                                <p className="text-lef"><strong>Clasificando en nivel:</strong> B.</p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+                <div className="slide">
+                    <Container>
+                        <Row>
+                            <Col xs="12">
+                                <h2 className="border border-bottom border-danger"><span>Calidad</span></h2>
+                                <TablaCalidad8 />
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </div>
+        )
+    }
+}
+
+class CalidadMarzo2 extends Component {
+    render() {
+        return (
+            <div >
+                <div className="slide">
+                    <Container>
+                        <Row>
+                            <Col xs="12">
+                                <h2 className="border border-bottom border-danger"><span>Calidad</span></h2>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs="6"><BarChart8 /></Col>
+                            <Col xs="6"><BarChart9 /></Col>
+                        </Row>
+                    </Container>
+                </div>
+                <div className="slide">
+                    <Container>
+                        <Row>
+                            <Col xs="12">
+                                <h2 className="border border-bottom border-danger"><span>Calidad</span></h2>
+                                <BarChartMar9 />
+                            </Col>
+                            <Col xs="12">
+                                <p className="text-justify">
+                                    Las principales Áreas de oportunidad detectadas son:<br />
+                                    <strong>Manejo de objeciones</strong>: Agente no debate las objeciones que
+                                    presenta el cliente.
+                                    <strong>Voz y vocabulario</strong>: Agente utiliza muletillas durante la
+                                     interacción con el cliente.<br />
+                                    <strong>Tipificación correcta</strong>: Agente tipifica la llamada de manera
+                                    incorrecta seleccionando una opción distinta a lo sucedido en la marcación.
+
+                                </p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+                <div className="slide">
+                    <Container>
+                        <Row>
+                            <Col xs="12">
+                                <h2 className="border border-bottom border-danger"><span>Calidad</span></h2>
+                                <BarChartDoble />
+                            </Col>
+                            <Col xs="4" className="text-left">
+                                <p>Con un promedio ABC de:<br />
+                                    <ul>
+                                        <li>92.08%</li>
+                                    </ul>
+                                    Clasificando en nivel:<br />
+                                    <ul>
+                                        <li>B</li>
+                                    </ul>
+                                </p>
+                            </Col>
+                            <Col xs="8" className="text-justify">
+                                <p>
+                                    Acciones:<br />
+                                    Refuerzo mediante clínica sobre la importancia de tipificar de forma correcta
+                                    la interacción.<br />
+                                    Clínica enfocada al manejo para debatir las negativas del cliente y poder colocar
+                                     una venta cruzada aprovechando en contacto.<br />
+                                     Taller de Voz y vocabulario para erradicar las multillas y extranjerismos para
+                                     poder tener una conversación profesional con el cliente.
+                                </p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </div>
+        )
+    }
+}
+
+export { Kpis, IndicadoresAtencion, ResumenVentasNuevas, CalidadMarzo1, CalidadMarzo2 }
